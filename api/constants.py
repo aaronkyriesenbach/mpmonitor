dbdd467fb5d1d253ce7f9ea7811b6bf77bef117e
Aaron Ky-Riesenbach <aaronkyriesenbach@pobox.com>
2024-01-17 16:27:24 -0500

    Initial commit to cloud branch

--- api/constants.py
+++
@@ -1,4 +0,0 @@
-DB_PATH = "database.db"
-FORUM_PATH = (
-    "https://www.mountainproject.com/forum/103989416/for-sale-for-free-want-to-buy"
-)
--- api/db.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-from constants import DB_PATH
-
-
-def get_db_conn():
-    conn = sqlite3.connect(DB_PATH)
-    conn.row_factory = sqlite3.Row
-    return conn
