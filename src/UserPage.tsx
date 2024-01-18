439810d7bb8e17521b04d257d4e50fdaf9fd87ac
Aaron Ky-Riesenbach <aaronkyriesenbach@pobox.com>
2024-01-18 14:21:01 -0500

    Add query guidelines

--- src/UserPage.tsx
+++ src/UserPage.tsx
@@ -62,6 +62,20 @@ export default function UserPage() {
           />
           <input type="submit" onClick={addQuery} />
         </div>
+
+        <br />
+
+        <div>
+          Guidelines on writing queries:
+          <ul>
+            <li>Queries are case-insensitive</li>
+            <li>Matching is performed exactly, so keep queries broad. For example "medium men's belay parka" will only match if the poster writes it exactly like that, so it might be better to just try "belay parka".</li>
+            <li>In the same vein, try keeping it singular. For example, the query "ice tool" will match both "ice tool" and "ice tools", but "ice tools" will only match the latter.</li>
+            <li>Make sure to match abbreviations as well as full names (i.e. use both "black diamond" and "bd")</li>
+          </ul>
+
+          Ask me (Aaron) if you have questions!
+        </div>
       </div>
     );
   } else {
